98682072fe76624e795d924038e15a1cab223b10
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2023-03-22 13:02:56 +0545

    Changed Database URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 module.exports = {
-    database: "mongodb+srv://sabin_lohani:sabinlohani123@cluster0.gnxokru.mongodb.net/?retryWrites=true&w=majority",
+    database: "mongodb+srv://sabin_lohani:1b3i3nSchwpVUz8Y@actors.viouy6d.mongodb.net/?retryWrites=true&w=majority",
     // database: 'mongodb://localhost:27017/waveDistributor',
     secret: crypto.randomBytes(32).toString("hex")
 };
