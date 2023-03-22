af498333907f69eaf43e9e7e01a38d717d7b49d5
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2023-03-22 11:22:19 +0545

    add mongodb link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
 module.exports = {
-    database: "mongodb://localhost:27017/waveDistributor",
+    database: "mongodb+srv://sabin_lohani:sabinlohani123@cluster0.gnxokru.mongodb.net/?retryWrites=true&w=majority",
     secret: crypto.randomBytes(32).toString("hex")
 };
