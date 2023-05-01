7939d6ac2311ca2d9ad134b128efea477374af3a
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2023-05-01 14:01:27 +0545

    Implemented fetch previous reading feature

--- routes/meterReaderRouter.js
+++ routes/meterReaderRouter.js
@@ -4,11 +4,12 @@ const meterReaderRouter = express.Router();
 const authenticateToken = require('../middlewares/authenticateToken');
 const isMeterReader = require('../middlewares/isMeterReader');
 
-const { fetchSchedule, fetchConsumers, addBill } = require('../controllers/meterReaderController');
+const { fetchSchedule, fetchConsumers, addBill, fetchPreviousReading } = require('../controllers/meterReaderController');
 
 // GET REQUESTS
 meterReaderRouter.get('/fetch-schedule', authenticateToken, isMeterReader, fetchSchedule);
 meterReaderRouter.get('/fetch-consumers', authenticateToken, isMeterReader, fetchConsumers)
+meterReaderRouter.get('/fetch-previous-reading', authenticateToken, isMeterReader, fetchPreviousReading);
 
 // POST REQUESTS
 meterReaderRouter.post('/add-bill', authenticateToken, isMeterReader, addBill);
